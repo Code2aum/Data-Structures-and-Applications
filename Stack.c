65c55bd6be7f80541426951fe5f498d6af5eede6
Harshith Ravi Kopparam <102581036+Code2aum@users.noreply.github.com>
2025-11-12 23:03:17 -0800

    Refactor main function
    Refactor main function for better readability and flow.

--- Stack.c
+++ Stack.c
@@ -4,41 +4,39 @@
 int top=-1,stack[20];
 void main()
 {
-int ch=1,option;
-clrscr();
-while(ch==1)
-{
-printf("Stack Operations\n");
-printf("1.Push\n2.Pop\n3.Display\n4.Exit\n");
-printf("Enter your choice : ");
-scanf("%d",&option);
-switch(option)
-{
-case 1:push();break;
-case 2:pop();break;
-case 3:display();break;
-case 4:exit(0);break;
-default :printf("Wrong choice\n");
-}
-printf("Do you want to continue 1-Yes,0-No : ");
-scanf("%d",&ch);
-}
+    int ch=1,option;
+    clrscr();
+    while(ch==1){
+        printf("Stack Operations\n");
+        printf("1.Push\n2.Pop\n3.Display\n4.Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d",&option);
+        switch(option)
+        {
+            case 1:push();break;
+            case 2:pop();break;
+            case 3:display();break;
+            case 4:exit(0);break;
+            default :printf("Wrong choice\n");
+        }
+        printf("Do you want to continue 1-Yes,0-No : ");
+        scanf("%d",&ch);
+    }
 }
 
 int push(){
     int num;
-if(top==(SIZE-1))
-{
-printf("Stack is full\n");
-return;
-}
-else
-{
-printf("Enter element to insert\n");
-scanf("%d",&num);
-stack[++top]=num;
-return;
-}
+    if(top==(SIZE-1))
+    {
+        printf("Stack is full\n");
+        return;
+    }
+    else{
+        printf("Enter element to insert\n");
+        scanf("%d",&num);
+        stack[++top]=num;
+        return;
+        }
 }
 int pop(){
     if(top==-1){
