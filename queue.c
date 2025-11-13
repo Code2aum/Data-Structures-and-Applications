4efd5c5c50b0ccda67c6bc894a09959cc40528ef
Harshith R K <code2aum@Harshiths-MacBook-Pro.local>
2025-11-12 23:07:02 -0800

    queue implementation

---
+++ queue.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+#define SIZE 20
+int rear=-1,front=-1,queue[10];
+void main()
+{
+int ch=1,option;
+clrscr();
+while(ch==1)
+{
+printf("Queue Operations\n");
+printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+printf("Enter your choice \n");
+scanf("%d",&option);
+switch(option)
+{
+case 1:qinsert();break;
+case 2:qdelete();break;
+case 3:qdisplay();break;
+case 4:exit(0);
+default :printf("Wrong choice\n");
+}
+printf("Do you want to continue 1-Yes,0-No\n");
+scanf("%d",&ch);
+}
+}
+
+int qinsert(){
+    int num;
+    if(rear==(SIZE-1)){
+    printf("Queue is full\n");
+    return;
+    }
+    printf("Enter element to insert\n");
+    scanf("%d",&num);
+    queue[++rear]=num;
+    if(front==-1){}
+front++;
+return;
+}
